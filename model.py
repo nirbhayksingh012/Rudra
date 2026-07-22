951218c186a9c9dfbde65f96b6362e0a050bb0fd
nirbhay singh <nirbhayksingh14@gmail.com>
2026-07-22 22:42:09 +0530

    fixing

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 # pyright: ignore[missing-import]
 import torch
+# pyrefly: ignore [missing-import]
 import torch.nn as nn
 from torch.nn import functional as F
 from config import block_size, n_embd, n_head, n_layer, dropout, device
